8032e43c699ac7d2df8eed1f173ef26d3b6cbdcc
James McCann <jamesredux@gmail.com>
2018-06-28 17:58:45 +0700

    using erb template and html

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,9 +1,9 @@
 require 'sinatra'
 require 'sinatra/reloader'
-	ran_num = rand(101)
+	number = rand(101)
 get '/' do 
 	
-	"The secret number is #{ran_num}."
+	erb :index, :locals => {:number => number}
 	
 end
 
