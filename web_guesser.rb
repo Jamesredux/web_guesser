9c7eba65d7df442e49b83b12dcb015a49c2c97c3
James McCann <jamesredux@gmail.com>
2018-06-30 18:24:13 +0700

    work on guess count

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,41 +1,63 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
+	@@guess_count = 0
 	SECRET_NUMBER = rand(101)
 
 	
 
 get '/' do 
 	guess = params['guess']
-	message = check_guess(guess)
-	color = get_color(guess)
-	erb :index, :locals => {:message => message, :color => color}
+	count = @@guess_count += 1
+	process_guess(guess)
+	diff = check_diff(guess)
+	message = check_guess(diff)
+	color = get_color(diff)
+
+	erb :index, :locals => {:message => message, :color => color, :count => count}
 
 	
 end
 
-
-def check_guess(guess)
-	guess_no = guess.to_i
-	if guess_no > SECRET_NUMBER
-		guess_no - SECRET_NUMBER > 5 ? "Way too high." : "Too high."
-	elsif guess_no < SECRET_NUMBER
-	 SECRET_NUMBER - guess_no > 5 ? "Way too low" : "Too  low!"
-	else guess_no == SECRET_NUMBER	
-		return "Correct! The secret number was #{SECRET_NUMBER}"
-	end	
+def process_guess(guess)
 	
 end
 
-def get_color(guess)
-	guess_int =  guess.to_i
-	diff = (SECRET_NUMBER - guess_int).abs
 
-	if diff > 5 
+def check_diff(guess)
+	guess_int = guess.to_i
+	diff = (SECRET_NUMBER - guess_int)
+	return diff
+end
+
+def check_guess(diff)
+	case diff
+
+	when -5..-1
+		"To high"
+	when 1..5
+		"Too Low"
+	when 0
+			"Correct! The secret number was #{SECRET_NUMBER}"
+	else
+		if diff < -5 
+			"Way too high"
+	 	elsif diff > 5
+	 		"Way too low"
+	 	end	
+	end					
+	
+end
+
+def get_color(diff)
+
+	true_diff = diff.abs 
+	
+	if true_diff > 5 
 		return "Red"
-	elsif diff == 0 
+	elsif true_diff == 0 
 		return "Green"
-	elsif diff < 5
+	elsif true_diff < 5
 	 return "LightPink"
 	end 
 	
