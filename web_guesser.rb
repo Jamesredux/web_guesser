e0067eea205198ac81f80036067d92988308eb6a
James McCann <jamesredux@gmail.com>
2018-07-01 10:49:02 +0700

    web guesser finished

--- web_guesser.rb
+++ web_guesser.rb
@@ -4,16 +4,19 @@ require 'sinatra/reloader'
 
 	
 	@@secret_number  = rand(101)
-	@@guessed = false
 	@@guess_count = 0
 
 
 get '/' do 
 	guess = params['guess']
-	process_guess(guess)
-	
+	cheat = params['cheat']
+	if cheat == 'true'
+		@message = "The code is #{@@secret_number}"
+	else	
+		process_guess(guess)
+	end
 
-	erb :index, :locals => {:message => @message, :color => @color, :count => @@guess_count, :number => @@secret_number}
+	erb :index, :locals => {:message => @message, :color => @color}
 
 	
 end
@@ -21,53 +24,48 @@ end
 
 
 def process_guess(guess)
-	@@guess_count += 1
 
+	@@guess_count += 1
 	diff = check_diff(guess)
 	@message = check_guess(diff)
-	@color = get_color(diff)
-	if @@guess_count > 3
+	if diff == 0 || @@guess_count > 5  
 		new_game
 	end	
-	
 end
 
 def new_game
 	@@secret_number  = rand(101)
-	@@guessed = false
 	@@guess_count = 0
-
-	
 end
 
 
 def check_diff(guess)
 	guess_int = guess.to_i
 	diff = (@@secret_number - guess_int)
-	return diff
+  diff
 end
 
 def check_guess(diff)
-	case diff
-
-	when -5..-1
-		"To high"
-	when 1..5
-		"Too Low"
-	when 0
-			"Correct! The secret number was #{@@secret_number}"
+	@color = get_color(diff)
+	if diff == 0 
+		"Correct! The secret number was #{@@secret_number}. A new number has been generated"
+	elsif @@guess_count > 5
+		"GAME OVER. You ran out of guesses. The number was #{@@secret_number} Please try again, a new number has been generated."
 	else
-		if diff < -5 
-			"Way too high"
-	 	elsif diff > 5
-	 		"Way too low"
-	 	end	
+		if diff > 5
+			"Way too low!"
+		elsif diff < -5
+			"Way too high!"
+		elsif diff > 0 
+			"Too low"
+		else
+			"Too high"
+		end				
 	end					
 	
 end
 
 def get_color(diff)
-
 	true_diff = diff.abs 
 	
 	if true_diff > 5 
@@ -81,8 +79,3 @@ def get_color(diff)
 end
 
 
-
-game = Game.new
-
-
-#currently on new branch testing guess count and a way to end game
